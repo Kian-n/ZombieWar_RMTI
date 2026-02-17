c9703f7f651e0b1037ac24e6632e86d8c7ad6ed9
Ibrahim Ali <alii12@csp.edu>
2026-02-17 17:38:19 -0600

    My part -
    Zombie parent class and child classes

---
+++ CommonInfected.java
@@ -0,0 +1,5 @@
+public class CommonInfected extends Zombie {
+    public CommonInfected() {
+        super(30, 5);
+    }
+}
---
+++ Zombie.java
@@ -0,0 +1,7 @@
+public class Zombie extends Character {
+
+    public Zombie(int health, int attackPower) {
+        this.health = health;
+        this.attackPower = attackPower;
+    }
+}
