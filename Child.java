1de276b62eb65a06405fe39dc2cc40e7b30c665f
taite-krogfus <krogfust@csp.edu>
2026-02-19 19:27:31 -0600

    Create Child.java

---
+++ Child.java
@@ -0,0 +1,10 @@
+/*
+* Child class that extends from Survivor
+* health and attackPower are declared, 20 and 2 respectively
+*/
+public class Child extends Survivor{
+    
+    public Child(){
+        super(20,2);
+    }
+}
