d62693174bc9113b00bc88c5bb87ce8eccd41490
taite-krogfus <krogfust@csp.edu>
2026-02-19 19:33:05 -0600

    Create Soldier.java

---
+++ Soldier.java
@@ -0,0 +1,11 @@
+/*
+* Soldier.java
+* Soldier class that extends from Survivor
+* health and attackPower are declared, 100 and 10 respectively
+ */
+public class Soldier extends Survivor{
+
+    public Soldier(){
+        super(100,10);
+    }
+}
